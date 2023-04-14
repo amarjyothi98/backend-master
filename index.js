f9e0e968b2edd18539fb08f2eb893959016f1c57
amarjyothi98 <amarjyothi526@gmail.com>
2023-04-14 13:00:42 +0530

    not completed Node.js basics at around 33 or 34 mins

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const http = require("http")
 
 // console.log(http)
 
-const server = http.createServer(() => {
-    console.log('served');
+const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    // res.end("<h1>This is Amar hehe</h1>"); 
+
+
+    // navigation of the pages
+    if(req.url==="/about") {
+        res.end("<h1>About page</h1>")
+    }
+    else if(req.url==="/profile") {
+        res.end("<h1>Profile page</h1>")
+    }
+    else if(req.url==="/") {
+        res.end("<h1>Home page</h1>")
+    }
+    else if(req.url==="/else") {
+        res.end("<h1>Else hehe page</h1>")
+    }
+    else {
+        res.end("<h1>page not found</h1>")
+    }
 })
 
 server.listen(5000, () => { 
