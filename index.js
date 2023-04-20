3f51ec875c51aaf9137322533d0aa0dbaac28455
amarjyothi98 <amarjyothi526@gmail.com>
2023-04-20 09:54:12 +0530

    completed ejs

--- index.js
+++ index.js
@@ -3,20 +3,44 @@ import path from "path";
 
 const app = express();  
 
+// setting up view engine
+app.set("view engine", "ejs")
+
+const users = [] ;
+
+// using middlewares
+// app.use(express.static(path.join(path.resolve(), "public")))
+app.use(express.urlencoded({ extended: true })); 
+
 app.get("/", (req, res) => {
     // res.json({
     //     success: true, 
     //     products: []
     // })
 
-    console.log(path.resolve())
-    const pathlocation = path.resolve(); 
+    // console.log(path.resolve())
+    // const pathlocation = path.resolve(); 
 
-    console.log(path.join(pathlocation, "./index.html"));
+    // console.log(path.join(pathlocation, "./index.html"));
+
     // res.sendFile(path.join(pathlocation, "./index.html")); 
 
-    
+    res.render("index", { name: "Amar" })
 
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body); 
+    
+    users.push({username:req.body.name, email:req.body.email}); 
+
+    res.render("success")
+})
+
+app.get("/users", (req, res) => {
+    res.json({
+        users,
+    })
 })
 
 app.listen(5000, () => {
